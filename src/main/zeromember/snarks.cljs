b48346791ade4b50d4a76d5333d5347e623d9510
Oskar Thoren <ot@oskarthoren.com>
2019-12-24 17:25:56 +0800

    Trusted setup and add keys

--- src/main/zeromember/snarks.cljs
+++ src/main/zeromember/snarks.cljs
@@ -6,6 +6,7 @@
 ;; fs only available in node, not in browser
 ;; so this needs to be served from http server, or generated in browser
 
+;; Load circuit
 (def circuit-def
   (.parse js/JSON
           (fs/readFileSync "snarks/circuit.json" "utf-8")))
@@ -13,4 +14,19 @@
 (def circuit
   (new zksnark/Circuit circuit-def))
 
-(.-nConstraints circuit)
+;; Inspect circuit
+;; (.-nConstraints circuit)
+
+(defn trusted-setup! [circuit]
+  (let [setup ((. zksnark/groth -setup) circuit)]
+    (do
+      (fs/writeFileSync
+        "snarks/circuit.vk_proof"
+        (.stringify js/JSON (zksnark/stringifyBigInts setup.vk_proof))
+        "utf-8")
+      (fs/writeFileSync
+        "snarks/circuit.vk_verifier"
+        (.stringify js/JSON (zksnark/stringifyBigInts setup.vk_verifier))
+        "utf-8"))))
+
+;; (trusted-setup! circuit)
