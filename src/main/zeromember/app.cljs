de1d1d39511477f6975c9e9eaf5562d41b9da83a
Oskar Thoren <ot@oskarthoren.com>
2019-12-24 12:52:48 +0800

    Add rum

--- src/main/zeromember/app.cljs
+++ src/main/zeromember/app.cljs
@@ -1,4 +1,26 @@
-(ns zeromember.app)
+(ns zeromember.app
+  (:require
+    [goog.dom :as gdom]
+    [rum.core :as rum]))
+
+(defonce app-state (atom {:text "Zeromember"}))
 
 (defn init []
   (println "Hello world"))
+
+(defn get-app-element []
+  (gdom/getElement "app"))
+
+(rum/defc hello-world []
+  [:div
+   [:h1 (:text @app-state)]
+   [:h3 "test ok"]])
+
+(defn mount [el]
+  (rum/mount (hello-world) el))
+
+(defn mount-app-element []
+  (when-let [el (get-app-element)]
+    (mount el)))
+
+(mount-app-element)
